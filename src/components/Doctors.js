7fbea201bda7530e2c59cb629b65f1716497630f
forLoop94 <charlesuwem94@gmail.com>
2023-09-28 19:59:51 +0100

    provide each doctor object an id property, to avoid using index as key

--- src/components/Doctors.js
+++ src/components/Doctors.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 function Doctors() {
   const doctors = [
     {
+      id: 1,
       name: 'Charles',
       specialization: 'Cardiology',
       city: 'Lagos',
@@ -11,6 +12,7 @@ function Doctors() {
       image: 'Image 99',
     },
     {
+      id: 2,
       name: 'Sandeep',
       specialization: 'Gynaecologist',
       city: 'New Delhi',
@@ -19,6 +21,7 @@ function Doctors() {
       image: 'Image 78',
     },
     {
+      id: 3,
       name: 'Emmauel',
       specialization: 'Orthopedics',
       city: 'Johannesburg',
@@ -27,13 +30,14 @@ function Doctors() {
       image: 'Image 24',
     },
     {
+      id: 4,
       name: 'Ariel',
       specialization: 'doctor',
       city: 'Buenos Ares',
       bio: "I don't need to know (necessarily) which people were involved. If so, how did you approach solving the conflict? If not, what measures did you put in place to prevent conflicts?",
       fee_per_appointment: '$580',
       image: 'Image 17',
-    }
+    },
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -59,19 +63,25 @@ function Doctors() {
       <h1>AVAILABLE DOCTORS</h1>
       <small>Please select from our list of doctors</small>
       <div className="slides">
-        {doctors.slice(currentIndex, currentIndex + 3).map((doctor, index) => (
-          <div key={index}>
+        {doctors.slice(currentIndex, currentIndex + 3).map((doctor) => (
+          <div key={doctor.id}>
             <div className="placeholder">{doctor.image}</div>
             <h3>{doctor.name}</h3>
             <div>
               <p>
-                City: {doctor.city}
+                City:
+                {' '}
+                {doctor.city}
               </p>
               <p>
-                Specialization: {doctor.specialization}
+                Specialization:
+                {' '}
+                {doctor.specialization}
               </p>
               <p>
-                Fee: {doctor.fee_per_appointment}
+                Fee:
+                {' '}
+                {doctor.fee_per_appointment}
               </p>
             </div>
             <p>{doctor.bio}</p>
@@ -86,4 +96,3 @@ function Doctors() {
 }
 
 export default Doctors;
-
