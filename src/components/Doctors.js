5f2dbbd55168414262a172c5389a1e22c2572d0b
forLoop94 <charlesuwem94@gmail.com>
2023-09-28 18:39:42 +0100

    Refactor Doctors component to display dynamic data

--- src/components/Doctors.js
+++ src/components/Doctors.js
@@ -2,15 +2,38 @@ import React, { useState } from 'react';
 
 function Doctors() {
   const doctors = [
-    'Doctor image 1',
-    'Doctor image 2',
-    'Doctor image 3',
-    'Doctor image 4',
-    'Doctor image 5',
-    'Doctor image 6',
-    'Doctor image 7',
-    'Doctor image 8',
-    'Doctor image 9',
+    {
+      name: 'Charles',
+      specialization: 'Cardiology',
+      city: 'Lagos',
+      bio: "I don't need to know (necessarily) which people were involved. If so, how did you approach solving the conflict? If not, what measures did you put in place to prevent conflicts?",
+      fee_per_appointment: '$145',
+      image: 'Image 99',
+    },
+    {
+      name: 'Sandeep',
+      specialization: 'Gynaecologist',
+      city: 'New Delhi',
+      bio: "I don't need to know (necessarily) which people were involved. If so, how did you approach solving the conflict? If not, what measures did you put in place to prevent conflicts?",
+      fee_per_appointment: '$200',
+      image: 'Image 78',
+    },
+    {
+      name: 'Emmauel',
+      specialization: 'Orthopedics',
+      city: 'Johannesburg',
+      bio: "I don't need to know (necessarily) which people were involved. If so, how did you approach solving the conflict? If not, what measures did you put in place to prevent conflicts?",
+      fee_per_appointment: '$178',
+      image: 'Image 24',
+    },
+    {
+      name: 'Ariel',
+      specialization: 'doctor',
+      city: 'Buenos Ares',
+      bio: "I don't need to know (necessarily) which people were involved. If so, how did you approach solving the conflict? If not, what measures did you put in place to prevent conflicts?",
+      fee_per_appointment: '$580',
+      image: 'Image 17',
+    },
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -37,13 +60,64 @@ function Doctors() {
       <h1>AVAILABLE DOCTORS</h1>
       <small>Please select from our list of doctors</small>
       <div className="slides">
-        <div className="placeholder first">{doctors[currentIndex]}</div>
-        <div className="placeholder second">{doctors[currentIndex + 1]}</div>
-        <div className="placeholder third">{doctors[currentIndex + 2]}</div>
+        <div>
+          <div className="placeholder first">{doctors[currentIndex].image}</div>
+          <h3>{doctors[currentIndex].name}</h3>
+          <div>
+            <p>
+              City:
+              {doctors[currentIndex].city}
+            </p>
+            <p>
+              specialization:
+              {doctors[currentIndex].specialization}
+            </p>
+            <p>
+              Fee:
+              {doctors[currentIndex].fee_per_appointment}
+            </p>
+          </div>
+          <p>{doctors[currentIndex].bio}</p>
+        </div>
+        <div className="second">
+          <div className="placeholder">{doctors[currentIndex + 1].image}</div>
+          <h3>{doctors[currentIndex + 1].name}</h3>
+          <div>
+            <p>
+              City:
+              {doctors[currentIndex + 1].city}
+            </p>
+            <p>
+              specialization:
+              {doctors[currentIndex + 1].specialization}
+            </p>
+            <p>
+              Fee:
+              {doctors[currentIndex + 1].fee_per_appointment}
+            </p>
+          </div>
+          <p>{doctors[currentIndex + 1].bio}</p>
+        </div>
+        <div className="third">
+          <div className="placeholder">{doctors[currentIndex + 2].image}</div>
+          <h3>{doctors[currentIndex + 2].name}</h3>
+          <div>
+            <p>
+              City:
+              {doctors[currentIndex + 2].city}
+            </p>
+            <p>
+              specialization:
+              {doctors[currentIndex + 2].specialization}
+            </p>
+            <p>
+              Fee:
+              {doctors[currentIndex + 2].fee_per_appointment}
+            </p>
+          </div>
+          <p>{doctors[currentIndex + 2].bio}</p>
+        </div>
       </div>
-      <h3>Doctor name</h3>
-      <small>hsjsdjvhdsufsdfsddsjgsadgwnssdgd</small>
-      <p>Doctor specialties</p>
 
       <button type="button" className="prev" onClick={prevSlide} style={prevButtonStyle}>&#10094;</button>
       <button type="button" className="next" onClick={nextSlide} style={nextButtonStyle}>&#10095;</button>
