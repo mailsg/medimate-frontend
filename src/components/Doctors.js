47c13564502c464da623957eeb339642d7500f78
forLoop94 <charlesuwem94@gmail.com>
2023-09-28 12:58:14 +0100

    Populate main page with dummy data, add font fam and colors

--- src/components/Doctors.js
+++ src/components/Doctors.js
@@ -2,15 +2,15 @@ import React, { useState } from 'react';
 
 function Doctors() {
   const doctors = [
-    'Doctor 1',
-    'Doctor 2',
-    'Doctor 3',
-    'Doctor 4',
-    'Doctor 5',
-    'Doctor 6',
-    'Doctor 7',
-    'Doctor 8',
-    'Doctor 9',
+    'Doctor image 1',
+    'Doctor image 2',
+    'Doctor image 3',
+    'Doctor image 4',
+    'Doctor image 5',
+    'Doctor image 6',
+    'Doctor image 7',
+    'Doctor image 8',
+    'Doctor image 9',
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -25,20 +25,25 @@ function Doctors() {
   };
 
   const prevButtonStyle = {
-    backgroundColor: currentIndex === 0 ? 'grey' : '#8DB600',
+    backgroundColor: currentIndex === 0 ? '#c3b9b9d3' : '#8DB600',
   };
 
   const nextButtonStyle = {
-    backgroundColor: currentIndex === doctors.length - 1 ? 'grey' : '#8DB600',
+    backgroundColor: currentIndex === doctors.length - 1 ? '#c3b9b9d3' : '#8DB600',
   };
 
   return (
     <div className="slideshow-container">
+      <h1>AVAILABLE DOCTORS</h1>
+      <small>Please select from our list of doctors</small>
       <div className="slides">
         <div className="placeholder first">{doctors[currentIndex]}</div>
         <div className="placeholder second">{doctors[currentIndex + 1]}</div>
         <div className="placeholder third">{doctors[currentIndex + 2]}</div>
       </div>
+      <h3>Doctor name</h3>
+      <small>hsjsdjvhdsufsdfsddsjgsadgwnssdgd</small>
+      <p>Doctor specialties</p>
 
       <button type="button" className="prev" onClick={prevSlide} style={prevButtonStyle}>&#10094;</button>
       <button type="button" className="next" onClick={nextSlide} style={nextButtonStyle}>&#10095;</button>
