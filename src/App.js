5d2923030a6d2ae65875ad96584bb97edcdcd787
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-10-02 13:35:21 +0200

    Add navigation

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable */
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+import Header from "./components/Header";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Header />}>
+      
+    </Route>
+  )
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <RouterProvider router={router} />
+    </main>
   );
 }
 
