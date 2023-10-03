e51b74058bfdb74c87eb5ef6f5f7052c99f1a5d3
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-10-03 02:41:00 +0200

    Add reserve

---
+++ src/components/Reserve.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import
+
+export const Reserve = () => (
+  <section className="container">
+    <article>
+      <h1>BOOK AN APPOINTMENT WITH OUR DOCTORS</h1>
+      <div className="thematic" />
+      <p>
+        Tell us about a time when you used the working agreements (or the roles)
+        that you set up at the beginning of the project, and how they helped you
+        work better as a team.
+      </p>
+      <div className="booking-btns">
+        <select>
+          <option>London</option>
+          <option>Mumbai</option>
+        </select>
+        <NavLink to="/Reservation">
+          <button type="button">Book now</button>
+        </NavLink>
+      </div>
+    </article>
+  </section>
+);
+
+export default Reserve;
