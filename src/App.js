a0ac4077045d2a2fe33005445d651a0d99e9cfbf
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-10-04 04:20:21 +0200

    Create signup component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Homepage from './routes/Homepage';
-import Login from './routes/Login';
-import SignUp from './routes/SignUp';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+} from 'react-router-dom';
+
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Login />} />
+      <Route path="signup" element={<SignUp />} />
+    </>,
+  ),
+);
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/homepage" element={<Homepage />} />
-        <Route path="/sign_up" element={<SignUp />} />
-      </Routes>
-    </div>
+    <main className="App">
+      <RouterProvider router={router} />
+    </main>
   );
 }
 
