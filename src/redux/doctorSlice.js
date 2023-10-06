b2230956941ccff70fc71b2cbf76598330e5f60c
forLoop94 <charlesuwem94@gmail.com>
2023-10-06 14:50:16 +0100

    Replace placeholders by fetching actual data from the database

--- src/redux/doctorSlice.js
+++ src/redux/doctorSlice.js
@@ -1,68 +1,20 @@
-import { createSlice } from '@reduxjs/toolkit';
-import image1 from '../assets/img-1.jpg';
-import image2 from '../assets/img-2.jpg';
-import image3 from '../assets/img-3.jpg';
-import image4 from '../assets/img-4.jpg';
-import image5 from '../assets/img-5.jpg';
-import image6 from '../assets/img-6.jpg';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const baseUrl = 'http://localhost:3000/api/v1';
+
+export const getDoctors = createAsyncThunk(
+  'get/doctors',
+  async () => {
+    const response = await axios.get(
+      `${baseUrl}/doctors}`,
+    );
+    return response.data;
+  },
+);
 
 const initialState = {
-  doctors: [
-    {
-      id: 1,
-      name: 'Laura',
-      specialization: 'Cardiology',
-      city: 'Lagos',
-      bio: "I don't need to know (necessarily) which people were involved.",
-      fee_per_appointment: '$145',
-      image: image1,
-    },
-    {
-      id: 2,
-      name: 'Condolezza',
-      specialization: 'Gynaecologist',
-      city: 'New Delhi',
-      bio: "I don't need to know (necessarily) which people were involved.",
-      fee_per_appointment: '$200',
-      image: image2,
-    },
-    {
-      id: 3,
-      name: 'Ruth',
-      specialization: 'Orthopedics',
-      city: 'Johannesburg',
-      bio: "I don't need to know (necessarily) which people were involved.",
-      fee_per_appointment: '$178',
-      image: image3,
-    },
-    {
-      id: 4,
-      name: 'Naomi',
-      specialization: 'doctor',
-      city: 'Buenos Ares',
-      bio: "I don't need to know (necessarily) which people were involved.",
-      fee_per_appointment: '$580',
-      image: image4,
-    },
-    {
-      id: 5,
-      name: 'Isabella',
-      specialization: 'doctor',
-      city: 'Buenos Ares',
-      bio: "I don't need to know (necessarily) which people were involved.",
-      fee_per_appointment: '$580',
-      image: image5,
-    },
-    {
-      id: 6,
-      name: 'Charles',
-      specialization: 'doctor',
-      city: 'Buenos Ares',
-      bio: "I don't need to know (necessarily) which people were involved.",
-      fee_per_appointment: '$580',
-      image: image6,
-    },
-  ],
+  doctors: [],
 };
 
 const DoctorSlice = createSlice({
@@ -70,6 +22,11 @@ const DoctorSlice = createSlice({
   initialState,
   reducers: {},
 
+  extraReducers: (builder) => {
+    builder.addCase(getDoctors.fulfilled, (state, action) => {
+      state.doctors = action.payload;
+    });
+  },
 });
 
 export default DoctorSlice.reducer;
