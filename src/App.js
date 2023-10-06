8aa3f35e0ea8a876bb65efbbdb746458bef941c3
forLoop94 <charlesuwem94@gmail.com>
2023-10-06 10:05:13 +0100

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-/* eslint-disable */
 import {
   createBrowserRouter,
   RouterProvider,
   Route,
   createRoutesFromElements,
-} from "react-router-dom";
+} from 'react-router-dom';
 
-import Header from "./components/Header";
-import Reserve from "./components/Reserve";
-import ReservationForm from "./components/ReservationForm";
+import Header from './components/Header';
+import Reserve from './components/Reserve';
+import ReservationForm from './components/ReservationForm';
 import Home from './components/Home';
 import Doctors from './components/Doctors';
 import Doctor from './components/Doctor';
-import './App.css'
-import SignUp from "./components/auth_pages/Signup";
-import Login from "./components/auth_pages/Login";
-import SignOut from "./components/auth_pages/Signout";
+import './App.css';
+import SignUp from './components/auth_pages/Signup';
+import Login from './components/auth_pages/Login';
+import SignOut from './components/auth_pages/Signout';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -29,8 +28,8 @@ const router = createBrowserRouter(
       <Route path="/sign_up" element={<SignUp />} />
       <Route path="/log_in" element={<Login />} />
       <Route path="/sign_out" element={<SignOut />} />
-    </>
-  )
+    </>,
+  ),
 );
 
 function App() {
