14f8dd7a4829d5bcd184726df40dcdf495623a0d
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-10-08 22:14:47 +0200

    Add appointments slice

--- src/redux/appointmentSlice.js
+++ src/redux/appointmentSlice.js
@@ -1,12 +1,34 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const initialState = [];
+const token = localStorage.getItem('token');
+export const getAppointments = createAsyncThunk('get/appointments', async () => {
+  const response = await axios.get('http://localhost:3000/api/v1/appointments', {
+    headers: {
+      Authorization: `${token}`,
+    },
+  });
+  return response.data;
+});
+
+const initialState = {
+  appointments: [],
+};
 
 const AppointmentSlice = createSlice({
   name: 'appointment',
   initialState,
-  reducers: {},
-
+  reducers: {
+  //   setReservations: (state, action) => {
+  //     state.reservations = action.payload;
+  //   },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getAppointments.fulfilled, (state, action) => {
+      const receivedData = action.payload;
+      return { ...state, appointments: receivedData };
+    });
+  },
 });
 
 export default AppointmentSlice.reducer;
