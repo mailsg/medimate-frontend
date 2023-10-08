9780ff3baa78fbf14a90421d33ca3d71b9c1cc66
Sandeep Ghosh <mailsg0912@gmail.com>
2023-10-08 16:00:03 +0530

    Fix routing bugs and GET/POST request issues

--- src/redux/doctorSlice.js
+++ src/redux/doctorSlice.js
@@ -1,14 +1,15 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3000/api/v1';
-
+const token = localStorage.getItem('token');
 export const getDoctors = createAsyncThunk(
   'get/doctors',
   async () => {
-    const response = await axios.get(
-      `${baseUrl}/doctors}`,
-    );
+    const response = await axios.get('http://localhost:3000/api/v1/doctors', {
+      headers: {
+        Authorization: `${token}`, // Replace with your JWT token
+      },
+    });
     return response.data;
   },
 );
@@ -20,11 +21,12 @@ const initialState = {
 const DoctorSlice = createSlice({
   name: 'doctor',
   initialState,
-  reducers: {},
+  // reducers: {},
 
   extraReducers: (builder) => {
     builder.addCase(getDoctors.fulfilled, (state, action) => {
-      state.doctors = action.payload;
+      const receivedData = action.payload;
+      return { ...state, doctors: receivedData };
     });
   },
 });
