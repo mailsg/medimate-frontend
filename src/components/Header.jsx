03c866a17f08935133380ce115bedad4b813ea52
forLoop94 <charlesuwem94@gmail.com>
2023-10-11 09:45:22 +0100

    Fix display errors in mobile menu and side panel

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,27 @@
 import { useState } from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import '../css/header.css';
 import {
-  BsVimeo, BsFacebook, BsPinterest, BsTwitter,
+  BsVimeo,
+  BsFacebook,
+  BsPinterest,
+  BsTwitter,
+  BsList,
 } from 'react-icons/bs';
 import { BiLogoGooglePlus } from 'react-icons/bi';
 import { toast } from 'react-toastify';
 import logo from '../images/Medimate-logo.png';
 
-export default function Header() {
+const Header = () => {
+  const path = useLocation();
+  const [panel, setPanel] = useState(true);
   const navigate = useNavigate();
 
-  const [menuVisible, setMenuVisible] = useState(false);
-
-  const toggleMenu = () => {
-    setMenuVisible(!menuVisible);
+  const setSidePanelBg = (targetPath) => {
+    if (path.pathname === targetPath) return { backgroundColor: 'var(--green)', color: 'white' };
+    return {};
   };
-
-  const handleKeyPress = (event) => {
-    if (event.key === 'Enter' || event.key === ' ') {
-      toggleMenu();
-    }
-  };
+  const setSidePanelClass = (targetPath) => (path.pathname === targetPath ? 'text-white' : '');
 
   const handleSignOut = async () => {
     try {
@@ -47,85 +48,125 @@ export default function Header() {
   };
 
   return (
-    <div className="side-panel">
-      <div className="logo-div">
-        <img src={logo} alt="logo" className="logo" />
+    <>
+      <div className="mobile-nav">
+        <span
+          role="button"
+          onClick={() => setPanel(!panel)}
+          onKeyDown={() => setPanel(!panel)}
+          tabIndex={0}
+        >
+          <BsList className="bars" />
+        </span>
       </div>
       <div
-        className={`menu-icon menu ${menuVisible ? 'open' : ''}`}
-        onClick={toggleMenu}
-        onKeyPress={handleKeyPress}
+        className={!panel ? 'show' : 'hide'}
+        id="header"
         role="button"
-        tabIndex="0"
-        aria-label="Toggle Menu"
+        onClick={() => setPanel(!panel)}
+        onKeyDown={() => setPanel(!panel)}
+        tabIndex={0}
       >
-        <div className="bar" />
-        <div className="bar" />
-        <div className="bar" />
-      </div>
-
-      <nav className={`side-nav ${menuVisible ? 'open' : ''}`}>
-        <NavLink
-          className="link-item1"
-          to="/home"
-          activeClassName="active"
-        >
-          Doctors
-        </NavLink>
-        <NavLink className="link-item2" to="Reserve" activeClassName="active">
-          Reserve
-        </NavLink>
-        <NavLink
-          className="link-item3"
-          to="Myreservations"
-          activeClassName="active"
-        >
-          My Reservations
-        </NavLink>
-        <NavLink className="link-item3" to="/home/Adddoctor" activeClassName="active">
-          Add Doctor
-        </NavLink>
-        <NavLink
-          className="link-item3"
-          to="Deletedoctor"
-          activeClassName="active"
-        >
-          Delete Doctor
-        </NavLink>
-        <div>
-          <button className="sign-out-btn" type="button" onClick={handleSignOut}>SIGN OUT</button>
+        <div className="nav-panel">
+          <div className="logo-container">
+            <img
+              height={150}
+              width={150}
+              src={logo}
+              alt="logo"
+              className="logo"
+            />
+          </div>
+          <ul>
+            <li style={setSidePanelBg('/')}>
+              <Link className={setSidePanelClass('/')} to="/">
+                Doctors
+              </Link>
+            </li>
+            <li style={setSidePanelBg('/reserve')}>
+              <Link className={setSidePanelClass('/reserve')} to="reserve">
+                Reserve
+              </Link>
+            </li>
+            <li style={setSidePanelBg('/reservations')}>
+              <Link
+                className={setSidePanelClass('/reservations')}
+                to="reservations"
+              >
+                Reservations
+              </Link>
+            </li>
+            <li style={setSidePanelBg('/add')}>
+              <Link className={setSidePanelClass('/add')} to="add">
+                Add Doctor
+              </Link>
+            </li>
+            <li style={setSidePanelBg('/delete')}>
+              <Link className={setSidePanelClass('/delete')} to="delete">Delete Doctor</Link>
+            </li>
+            <li>
+              <button
+                type="button"
+                onClick={handleSignOut}
+                className="sign-out"
+              >
+                SIGN OUT
+              </button>
+            </li>
+          </ul>
         </div>
-      </nav>
-      <footer className="footer">
-        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-          <BsTwitter />
-        </a>
-        <a
-          href="https://www.facebook.com"
-          target="_blank"
-          rel="noopener noreferrer"
+        <footer className="footer">
+          <div>
+            <a
+              href="https://twitter.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <BsTwitter />
+            </a>
+            <a
+              href="https://www.facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <BsFacebook />
+            </a>
+            <a
+              href="https://plus.google.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <BiLogoGooglePlus />
+            </a>
+            <a
+              href="https://vimeo.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <BsVimeo />
+            </a>
+            <a
+              href="https://www.pinterest.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <BsPinterest />
+            </a>
+          </div>
+          <p>© 1989 Medimate - All Rights Reserved</p>
+        </footer>
+        <div
+          className="mobile close"
+          role="button"
+          onClick={() => setPanel(!panel)}
+          onKeyDown={() => setPanel(!panel)}
+          tabIndex={0}
         >
-          <BsFacebook />
-        </a>
-        <a
-          href="https://plus.google.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <BiLogoGooglePlus />
-        </a>
-        <a href="https://vimeo.com" target="_blank" rel="noopener noreferrer">
-          <BsVimeo />
-        </a>
-        <a
-          href="https://www.pinterest.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <BsPinterest />
-        </a>
-        <p>© 1989 Medimate - All Rights Reserved</p>
-      </footer>
-    </div>
+          <i className="fa fa-solid fa-xmark" />
+        </div>
+      </div>
+    </>
   );
-}
+};
+
+export default Header;
